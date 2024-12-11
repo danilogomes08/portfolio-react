7d8c3b69942c5af3d596c4eff6532e7b0deba8e2
Danilo Gomes <danilo.dsgomes@gmail.com>
2024-12-11 12:43:18 -0300

    atualizado sobre

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -19,11 +19,14 @@ const About = props => {
 
                 <p>
                     Olá! Sou um Desenvolvedor Front-End, formado pela FATEC Santos, com paixão por criar interfaces dinâmicas, funcionais e atraentes para web. Minha missão é transformar ideias em experiências digitais que encantam e entregam valor.
-                  
+                </p>  
+                <p>
                     Com uma sólida experiência em tecnologias modernas, domino ferramentas e linguagens como React, JavaScript, HTML, CSS, SASS e Bootstrap. Além disso, possuo expertise em design e desenvolvimento com Elementor PRO, o que me permite construir sites personalizados e altamente otimizados no WordPress.
-                    
+                </p>
+                <p>    
                     Valorizo a atenção aos detalhes, buscando sempre estar atualizado com as melhores práticas e tendências do mercado. Seja para projetos corporativos, startups ou iniciativas pessoais, estou preparado para entregar soluções eficientes e criativas.
-                    
+                </p>
+                <p>    
                     Vamos criar algo incrível juntos?
                 </p>
 
