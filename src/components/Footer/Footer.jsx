69d274a4fabb14156ee2817c0f4e0d35ce97ebc5
Danilo Gomes <danilo.dsgomes@gmail.com>
2025-05-02 21:55:33 -0300

    atualizado portfolio

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = _ => {
     return (
         <footer>
             <p>
-                © Danilo Portfolio - 2024
+                © Danilo Portfolio - 2025
             </p>
         </footer>
     )
